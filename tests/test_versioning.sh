f7cb867161314066106b4290e361c7819ab5feb3
Pieter Lexis <pieter@plexis.eu>
2018-04-03 09:59:12 +0200

    Fix issues with 'dirty' versions

--- tests/test_versioning.sh
+++ tests/test_versioning.sh
@@ -14,29 +14,53 @@ source "helpers/functions.sh"
 builder_release='1pdns'
 
 src_versions=(1.0.0
+              1.0.0.dirty
               1.0.0-beta1
+              1.0.0-beta1.dirty
               1.1.0-rc2.0.g123456
+              1.1.0-rc2.0.g123456.dirty
               1.1.0.15.g123456
+              1.1.0.15.g123456.dirty
               1.1.0.15.branchname.g123456
-              1.2.0-alpha1.10.branch.g123456)
+              1.1.0.15.branchname.g123456.dirty
+              1.2.0-alpha1.10.branch.g123456
+              1.2.0-alpha1.10.branch.g123456.dirty)
 deb_versions=(1.0.0
+              1.0.0+dirty
               1.0.0~beta1
+              1.0.0~beta1+dirty
               1.1.0~rc2+0.g123456
+              1.1.0~rc2+0.g123456.dirty
               1.1.0+15.g123456
+              1.1.0+15.g123456.dirty
               1.1.0+branchname.15.g123456
-              1.2.0~alpha1+branch.10.g123456)
+              1.1.0+branchname.15.g123456.dirty
+              1.2.0~alpha1+branch.10.g123456
+              1.2.0~alpha1+branch.10.g123456.dirty)
 rpm_versions=(1.0.0
+              1.0.0
+              1.0.0
               1.0.0
               1.1.0
               1.1.0
               1.1.0
+              1.1.0
+              1.1.0
+              1.1.0
+              1.2.0
               1.2.0)
 rpm_releases=($builder_release
+              dirty.$builder_release
               0.beta1.$builder_release
+              0.beta1.dirty.$builder_release
               0.rc2.0.g123456.$builder_release
+              0.rc2.0.g123456.dirty.$builder_release
               15.g123456.$builder_release
+              15.g123456.dirty.$builder_release
               branchname.15.g123456.$builder_release
-              0.alpha1.branch.10.g123456.$builder_release)
+              branchname.15.g123456.dirty.$builder_release
+              0.alpha1.branch.10.g123456.$builder_release
+              0.alpha1.branch.10.g123456.dirty.$builder_release)
 
 for ctr in ${!src_versions[@]}; do
   BUILDER_VERSION=${src_versions[$ctr]}
