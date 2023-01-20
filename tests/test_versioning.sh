cc7ed4f8f9cacb0a1521afc115f6081c4fc1abb5
Konrad Wojas <github@m.wojas.nl>
2023-01-20 13:27:29 +0800

    Make BUILDER_PYTHON_SRC_VERSION PEP 440 compliant
    Make BUILDER_PYTHON_SRC_VERSION PEP 440 compliant.
    
    Setuptools 66 (Jan 2023) turned version format warnings into errors,
    breaking builds.

--- tests/test_versioning.sh
+++ tests/test_versioning.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+exitcode=0
 assert_equal() {
-  if [ "$1" != "$2" ]; then
-    echo "${1} != ${2}"
-    exit 1
+  if [ "$2" != "$3" ]; then
+    echo "${1}: ${2} != ${3}"
+    exitcode=1
   fi
 }
 
@@ -62,34 +63,36 @@ rpm_releases=($builder_release
               0.alpha1.branch.10.g123456.$builder_release
               0.alpha1.branch.10.g123456.dirty.$builder_release)
 
-# note, these do not comply to PEP 440
+# These comply to PEP 440
 py_versions=(1.0.0
-             1.0.0.dirty
+             1.0.0+dirty
              1.0.0b1
-             1.0.0-beta1.dirty
-             1.1.0-rc2.0.g123456
-             1.1.0-rc2.0.g123456.dirty
-             1.1.0.15.g123456
-             1.1.0.15.g123456.dirty
-             1.1.0.15.branchname.g123456
-             1.1.0.15.branchname.g123456.dirty
-             1.2.0-alpha1.10.branch.g123456
-             1.2.0-alpha1.10.branch.g123456.dirty)
+             1.0.0b1+dirty
+             1.1.0rc2+0.g123456
+             1.1.0rc2+0.g123456.dirty
+             1.1.0+15.g123456
+             1.1.0+15.g123456.dirty
+             1.1.0+15.branchname.g123456
+             1.1.0+15.branchname.g123456.dirty
+             1.2.0a1+10.branch.g123456
+             1.2.0a1+10.branch.g123456.dirty)
 
 for ctr in ${!src_versions[@]}; do
   BUILDER_VERSION=${src_versions[$ctr]}
   BUILDER_RELEASE=$builder_release
   set_debian_versions
-  assert_equal $BUILDER_DEB_VERSION ${deb_versions[$ctr]}
-  assert_equal $BUILDER_DEB_RELEASE $builder_release
+  assert_equal BUILDER_DEB_VERSION $BUILDER_DEB_VERSION ${deb_versions[$ctr]}
+  assert_equal BUILDER_DEB_RELEASE $BUILDER_DEB_RELEASE $builder_release
 
   set_rpm_versions
-  assert_equal $BUILDER_RPM_VERSION ${rpm_versions[$ctr]}
-  assert_equal $BUILDER_RPM_RELEASE ${rpm_releases[$ctr]}
+  assert_equal BUILDER_RPM_VERSION $BUILDER_RPM_VERSION ${rpm_versions[$ctr]}
+  assert_equal BUILDER_RPM_RELEASE $BUILDER_RPM_RELEASE ${rpm_releases[$ctr]}
 
   set_python_src_versions
-  assert_equal $BUILDER_PYTHON_SRC_VERSION ${py_versions[$ctr]}
+  assert_equal BUILDER_PYTHON_SRC_VERSION $BUILDER_PYTHON_SRC_VERSION ${py_versions[$ctr]}
 
-  assert_equal $BUILDER_VERSION ${src_versions[$ctr]}
-  assert_equal $BUILDER_RELEASE $builder_release
+  assert_equal BUILDER_VERSION $BUILDER_VERSION ${src_versions[$ctr]}
+  assert_equal BUILDER_RELEASE $BUILDER_RELEASE $builder_release
 done
+
+exit "$exitcode"
