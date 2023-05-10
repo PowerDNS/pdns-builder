a7bc551be89bada3ae7eb79598368a1e5d44036b
Konrad Wojas <github@m.wojas.nl>
2023-05-10 17:12:19 +0800

    Fix tests: replace centos-8 by rocky-8

--- tests/test-rocky-8-reproducible.sh
+++ tests/test-rocky-8-reproducible.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-# Test if centos-8 RPM builds are reproducible
+# Test if rcoky-8 RPM builds are reproducible
 # Must be run from demo dir
 
 set -ex
 
 # First build
-./builder/build.sh -B MYCOOLARG=iLikeTests centos-8
+./builder/build.sh -B MYCOOLARG=iLikeTests rocky-8
 
 # Record hashes
 sha256sum \
-    builder/tmp/latest/centos-8/dist/noarch/*.rpm \
+    builder/tmp/latest/rocky-8/dist/noarch/*.rpm \
     builder/tmp/latest/sdist/*.tar.gz \
     > /tmp/sha256sum.txt
 
 # Second build after cleaning and adding a file to invalidate the build context
-rm -rf ./builder/tmp/latest/centos-8
+rm -rf ./builder/tmp/latest/rocky-8
 rm -rf ./builder/tmp/latest/sdist
-./builder/build.sh -B MYCOOLARG=iLikeTests -b build-again centos-8
+./builder/build.sh -B MYCOOLARG=iLikeTests -b build-again rocky-8
 
 # Check hashes, should be identical
 sha256sum -c /tmp/sha256sum.txt
