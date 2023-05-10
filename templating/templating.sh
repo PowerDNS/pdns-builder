1d2b94f10c9c9abd8a1d7d0e217f253cd2087357
Konrad Wojas <github@m.wojas.nl>
2023-05-10 16:14:26 +0800

    Templates: support nested @IF statements

--- templating/templating.sh
+++ templating/templating.sh
@@ -12,7 +12,7 @@ if [ "$1" = "" ]; then
     echo '@EXEC uname -a'
     echo '@EXEC [ "$foo" = "bar" ] && include bar.txt'
     echo '@IF [ "$foo" = "bar" ]'
-    echo 'This line is only printed if $foo = "bar" (cannot be nested)'
+    echo 'This line is only printed if $foo = "bar"'
     echo '@ENDIF'
     echo 'Other lines are printed unchanged.'
     echo
@@ -28,15 +28,17 @@ tmpl_prefix=${tmpl_prefix:-@}
 
 include() {
     [ "$tmpl_debug" != "" ] && echo "$tmpl_comment $1"
-    local skip
+    local skip=0
     local line
     local condition
     ( cat "$1" && echo ) | while IFS= read -r line; do
         if [[ $line = ${tmpl_prefix}ENDIF* ]]; then
-            skip=
+            # Only decrement if larger than 0, because it was only
+            # incremented if the block was disabled.
+            [ $skip -gt 0 ] && skip=$((skip-1))
             [ "$tmpl_debug" != "" ] && echo "$tmpl_comment $line"
 
-        elif [ ! -z "$skip" ]; then
+        elif [ $skip -gt 0 ]; then
             # nothing, in IF that evaluated to false
             [ "$tmpl_debug" != "" ] && echo "$tmpl_comment     $line"
             
@@ -56,7 +58,7 @@ include() {
         elif [[ $line = ${tmpl_prefix}IF\ * ]]; then
             condition=${line#* }
             [ "$tmpl_debug" != "" ] && echo "$tmpl_comment $line"
-            eval "$condition" || skip=1
+            eval "$condition" || skip=$((skip+1))
 
         else
             echo "$line"
